baee1f2759335e76f4d59ea175950f227b642ec1
Kurtis Bassmann <knbsk8@gmail.com>
2018-12-04 23:55:06 -0500

    Gets the price of BTC once, when any button is pressed LTC price is displayed, not the price of the coin indicated by the button

--- main.js
+++ main.js
@@ -1,37 +1,71 @@
-document.addEventListener("DOMContentLoaded", function(event) {
+document.addEventListener("DOMContentLoaded", function (event) {
 
-function httpGet(theUrl)
-{
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
-    xmlHttp.send( null );
-    return xmlHttp.responseText;
-}
+    main();
 
-function runGet() {
-    var strResponse = httpGet('https://api.coinbase.com/v2/prices/BTC-USD/buy');
-    var requestJson = JSON.parse(strResponse);
-    var data = requestJson['data']
 
-    console.log(data)
+    function httpGet(theUrl) {
+        var xmlHttp = new XMLHttpRequest();
+        xmlHttp.open("GET", theUrl, false); // false for synchronous request
+        xmlHttp.send(null);
+        return xmlHttp.responseText;
+    }
 
-    var price = data['amount']
-    var coin = data['base']
-    var currency= data['currency']
-    // var information = (price + " " + currency + " For " + coin);
-    return [price, coin, currency]
-}
+    function fetch(coin) {
+        var strResponse = httpGet('https://api.coinbase.com/v2/prices/' + coin + '-USD/buy');
+        var requestJson = JSON.parse(strResponse);
+        var data = requestJson['data']
+        var price = data['amount']
+        var coin = data['base']
+        var currency = data['currency']
+        return [price, coin, currency];
+    }
 
-function main() {
-    doc = document;
-    var info = runGet();
+    function addListeners() {
+        var bar = $('#bar');
+        var buttons = bar.children();
+        // console.log(buttons)
+        for (var i = 0; i < 3; i++) {
+            var button = buttons.eq(i);
+            console.log(button)
+            var coin = button.text();
+            button.click(function(){
+                console.log(coin, "COIN")
+                var info = fetch(coin);
+                update(info[0], info[1], info[2])
+            });
+        }
+    }
 
-    var priceEle = $('#price')
-    priceEle.html(info[0] + " " + info[2])
+    function update(price, coin, currency) {
+        //update text
+        console.log("updating " + coin + " at " + price)
+        var priceEle = $('#price')
+        priceEle.html(price + " " + currency)
 
-    var coinEle = $('#coin')
-    coinEle.html(info[1])
-}
+        var coinEle = $('#coin')
+        coinEle.html(coin)
+    }
 
-main();
+
+    function main() {
+        addListeners();
+        var info = fetch('btc');
+        update(info[0], info[1], info[2]);
+    }
+
+
 });
+// function main() {
+//     var bar = $('#bar');
+//     var buttons = bar.children();
+//     // console.log(buttons)
+//     for(var i = 0; i < 3; i++){
+//         var button = buttons.eq(i);
+//         var coin = button.text();
+//         button.click(function(coin){
+//             update(coin)}
+//             );
+//     }
+//     setInterval(function(){update(coin)}, 1000 * 1.5)
+// }
+// main();
